2f814923cc4bb4b41d4f48485a8f41ed175c976b
Vladimír Mach <vmach@aps-holding.com>
2015-09-18 16:04:17 +0200

    Rename solution.

--- Mach.Utils.MultimediaKeysMapper/Properties/AssemblyInfo.cs
+++ Mach.Utils.MultimediaKeysMapper/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MultimediaKeysMapper")]
+[assembly: AssemblyTitle("Mach.Utils.MultimediaKeysMapper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MultimediaKeysMapper")]
+[assembly: AssemblyCompany("Vladimír Mach")]
+[assembly: AssemblyProduct("Mach.Utils.MultimediaKeysMapper")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Mach.Utils.MultimediaKeysMapper/Properties/Resources.Designer.cs
+++ Mach.Utils.MultimediaKeysMapper/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MultimediaKeysMapper.Properties {
+namespace Mach.Utils.MultimediaKeysMapper.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MultimediaKeysMapper.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MultimediaKeysMapper.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mach.Utils.MultimediaKeysMapper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
